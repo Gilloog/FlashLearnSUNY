a3e756b0cc541fc44a6645c2cb2b031f0b6bade5
Gavin Gillooley <gilloog@sunypoly.edu>
2024-10-25 09:14:32 -0400

    modifiy auth.py

--- auth.py
+++ auth.py
@@ -1,15 +1,13 @@
 import sqlite3
-import hashlib
 from database import get_db_connection
 
 def register_user(username, password):
     con = get_db_connection()
     cursor = con.cursor()
     
-    hashed_password = hashlib.sha256(password.encode()).hexdigest()
     
     try: 
-        cursor.execute('INSERT INTO users (username, password) VALUES (?,?)', (username, hashed_password))
+        cursor.execute('INSERT INTO users (username, password) VALUES (?,?)', (username, password))
         con.commit()
         print("User has been registered.")
         return True
@@ -23,9 +21,8 @@ def login_user(username, password):
     con = get_db_connection()
     cursor = con.cursor()
     
-    hashed_password = hashlib.sha256(password.encode()).hexdigest()
     
-    cursor.execute('SELECT * FROM users WHERE username = ? AND password = ?', (username, hashed_password)) 
+    cursor.execute('SELECT * FROM users WHERE username = ? AND password = ?', (username, password)) 
     user = cursor.fetchone()
     con.close()
     
